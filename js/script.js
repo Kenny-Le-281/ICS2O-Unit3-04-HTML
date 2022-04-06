a4655af3494a72bab64bdef20cb854b21fdc5cc5
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-06 17:05:43 +0000

    added calculations to script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 /* Created by: Kenny Le
- * Created on: Mar 2022
+ * Created on: Apr 2022
  * This file contains the JS functions for index.html 
 */
+
+'use strict'
+
+/**
+ * This function converts Fahrenheit to Celsius
+ */
+function convert () {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('celsius').innerHTML = fahrenheit + '℉ in celsius is equal to: ' + celsius.toFixed(2) + '℃'
+}
